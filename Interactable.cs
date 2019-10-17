afe8024b4f747b17e8204f1ac06ac3f3875377fc
RobinLmn <55120018+RobinLmn@users.noreply.github.com>
2019-10-16 22:46:54 -0400

    Add files via upload

---
+++ Interactable.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 3f;               // How close do we need to be to interact?
+    public Transform interactionTransform;  // The transform from where we interact in case you want to offset it
+    public Transform player;
+
+    public virtual void Interact()
+    {
+        // This method is meant to be overwritten
+        Debug.Log("Interacting with " + transform.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(player.position, interactionTransform.position);
+
+        if ((distance <= radius) && Input.GetKeyDown(KeyCode.E))
+        {
+            // Interact with the object
+            Interact();
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
+    }
+
+
+}
