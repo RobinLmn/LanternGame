81ae0317a13acaf17b398d2ec87f9c90f9b64b3b
RobinLmn <55120018+RobinLmn@users.noreply.github.com>
2019-11-06 01:25:14 -0500

    Tried to implement Candies
    Failed as :
    
    - Everytime you eat another candy the count restarts at 0, we have to separate the count and the object
    
    - The animation doesn't start well

--- Lantern Game Unity/Assets/Scripts/Interactable.cs
+++ Lantern Game Unity/Assets/Scripts/Interactable.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Interactable : MonoBehaviour
 {
     public float radius = 3f;               // How close do we need to be to interact?
     public Transform interactionTransform;  // The transform from where we interact in case you want to offset it
     public Transform player;
+    public int lantern_number;
+    public Animator animator;
+    public TextMeshProUGUI scoreText;
 
-    public virtual void Interact()
+
+    public void Awake()
     {
-        // This method is meant to be overwritten
-        Debug.Log("Interacting with " + transform.name);
+        lantern_number = 0;
+        SetScoreText();
     }
 
     // Update is called once per frame
@@ -26,11 +31,40 @@ public class Interactable : MonoBehaviour
         }
     }
 
+    private void Interact()
+    {
+        IncreaseLantern();
+        PickUp();   // Pick it up!
+        SetScoreText();
+    }
+
+    // Pick up the item
+    void PickUp()
+    {
+        Debug.Log("Picking up Candy");
+        bool wasPickedUp = true;
+
+        // If successfully picked up
+        if (wasPickedUp)
+            Destroy(gameObject);    // Destroy item from scene
+    }
+
+
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(interactionTransform.position, radius);
     }
 
+    private void IncreaseLantern()
+    {
+        Debug.Log("IncreaseLantern");
+        lantern_number += 1;
+        animator.SetInteger("lanternlevel", lantern_number);
+    }
 
+    private void SetScoreText()
+    {
+        scoreText.text = lantern_number.ToString();
+    }
 }
